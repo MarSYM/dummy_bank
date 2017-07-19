c184498fc899cad3a655c5f9478dd53054b50846
MarSYM <snow.waltz55@gmail.com>
2017-07-19 18:27:55 +0700

    [NEW] test drawdown repository

--- src/main/java/api/DrawdownTransRepository.java
+++ src/main/java/api/DrawdownTransRepository.java
@@ -1,5 +1,28 @@
 package api;
 
-public class DrawdownTransRepository {
+import java.util.ArrayList;
+import java.util.List;
 
+public class DrawdownTransRepository {
+	
+	public static List<DrawdownTrans> ddTransList = new ArrayList<DrawdownTrans>();
+	
+	
+	public void setDrawdownTrans(DrawdownTrans drawdownTrans){
+		System.out.println("size "+ddTransList.size());
+		if(ddTransList.size()==0){
+			System.out.println("size 0 "+ddTransList.size());
+			ddTransList.add(drawdownTrans);
+		}
+		else {
+			System.out.println("else "+ddTransList.size());
+			ddTransList.set(0, drawdownTrans);
+		}
+			
+	}
+	
+	public DrawdownTrans getDrawdownTrans(){
+		
+		return ddTransList.get(0);
+	}
 }
---
+++ src/test/java/api/DrawdownTransRepositoryTest.java
@@ -0,0 +1,59 @@
+package api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DrawdownTransRepositoryTest {
+
+	@Test
+	public void when_resave_then_should_return_new_value() {
+		DrawdownTrans drawdownTrans = new DrawdownTrans();
+		
+		drawdownTrans.setDelayResponseTime("1");
+		drawdownTrans.setTradeNavigatorTransactionNo("T111132323");
+		drawdownTrans.setTradeNavigatorTransactionTimeStamp("20160717104300");
+		drawdownTrans.setInterestAmount("500");
+		drawdownTrans.setRepaymentFee("20");
+		drawdownTrans.setReturnStatus("BE");
+		drawdownTrans.setReturnCode("0000");
+		drawdownTrans.setReturnMessage("test");
+		
+		DrawdownTransRepository drawdownTransRepository = new DrawdownTransRepository();
+		drawdownTransRepository.setDrawdownTrans(drawdownTrans);
+		DrawdownTrans resultDrawdown = drawdownTransRepository.getDrawdownTrans();
+		
+		assertEquals("1",resultDrawdown.getDelayResponseTime());
+		assertEquals("T111132323",resultDrawdown.getTradeNavigatorTransactionNo());
+		assertEquals("20160717104300",resultDrawdown.getTradeNavigatorTransactionTimeStamp());
+		assertEquals("500",resultDrawdown.getInterestAmount());
+		assertEquals("20",resultDrawdown.getRepaymentFee());
+		assertEquals("BE",resultDrawdown.getReturnStatus());
+		assertEquals("0000",resultDrawdown.getReturnCode());
+		assertEquals("test",resultDrawdown.getReturnMessage());
+		
+		DrawdownTrans drawdownTrans2 = new DrawdownTrans();
+		
+		drawdownTrans2.setDelayResponseTime("2");
+		drawdownTrans2.setTradeNavigatorTransactionNo("T1111323232");
+		drawdownTrans2.setTradeNavigatorTransactionTimeStamp("201607171043002");
+		drawdownTrans2.setInterestAmount("5002");
+		drawdownTrans2.setRepaymentFee("202");
+		drawdownTrans2.setReturnStatus("BE2");
+		drawdownTrans2.setReturnCode("00002");
+		drawdownTrans2.setReturnMessage("test2");
+		
+		drawdownTransRepository.setDrawdownTrans(drawdownTrans2);
+		DrawdownTrans resultDrawdown2 = drawdownTransRepository.getDrawdownTrans();
+		
+		assertEquals("12",resultDrawdown2.getDelayResponseTime());
+		assertEquals("T1111323232",resultDrawdown2.getTradeNavigatorTransactionNo());
+		assertEquals("201607171043002",resultDrawdown2.getTradeNavigatorTransactionTimeStamp());
+		assertEquals("5002",resultDrawdown2.getInterestAmount());
+		assertEquals("202",resultDrawdown2.getRepaymentFee());
+		assertEquals("BE2",resultDrawdown2.getReturnStatus());
+		assertEquals("00002",resultDrawdown2.getReturnCode());
+		assertEquals("test2",resultDrawdown2.getReturnMessage());
+	}
+
+}
